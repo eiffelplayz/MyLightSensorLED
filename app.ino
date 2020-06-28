d1b515b95958747f7811875b4682fb660640f631
eiffelplayz <eiffelplayz@gmail.com>
2020-06-28 08:36:51 +0800

    Add comments

--- app.ino
+++ app.ino
@@ -7,21 +7,18 @@ void setup() {
 void loop()
 {
     // Read photo resistor value from A1 pin
-	int light = analogRead(A1);
-    // The map() function uses integer math so will not generate fractions, 
-    // when the math might indicate that it should do so. 
-    // Fractional remainders are truncated, and are not rounded or averaged.
-    // map(value, fromLow, fromHigh, toLow, toHigh)
+    int light = analogRead(A1);
+    // Programmaticaly set the light value range [400..900]
     if( light < 400 ) light = 400;
     if ( light > 900 ) light = 900;
-
+    // Calculate LED brightness range [0..255] by map function
     int brightness = map(light, 400, 900, 255, 0);
     
     Serial.print(light);
     Serial.print(" => ");
     Serial.println(brightness);
     
-    // Analog write to LED D9 pin
+    // Analog write brightness to LED D9 pin
     analogWrite(9, brightness); 
     delay(2);
 }
